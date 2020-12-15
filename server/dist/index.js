24c519f5d2c6d34b861f5d168092aa807e87e27e
famanoder <915905174@qq.com>
2020-12-15 18:41:27 +0800

    feat: rewrite with apollo-server

---
+++ server/dist/index.js
@@ -0,0 +1,40 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const koa_1 = __importDefault(require("koa"));
+const apollo_server_koa_1 = require("apollo-server-koa");
+const typeDefs = apollo_server_koa_1.gql `
+  type Task {
+    name: String
+    time: Int
+  }
+  type Query {
+    hello: String
+    hh: Int
+    tasks(key: String): [Task]
+  }
+`;
+const resolvers = {
+    Query: {
+        hello: () => `({
+      name: '111'
+    })`,
+        hh: () => 1,
+        tasks(a, o) {
+            return [
+                {
+                    name: o.key,
+                    time: 12345
+                }
+            ];
+        }
+    }
+};
+const server = new apollo_server_koa_1.ApolloServer({ typeDefs, resolvers,
+    tracing: true });
+const app = new koa_1.default();
+const port = 4000;
+server.applyMiddleware({ app });
+app.listen({ port }, () => console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`));
---
+++ server/src/index.ts
@@ -0,0 +1,42 @@
+import Koa from 'koa';
+import { ApolloServer, gql } from 'apollo-server-koa';
+ 
+const typeDefs = gql`
+  type Task {
+    name: String
+    time: Int
+  }
+  type Query {
+    hello: String
+    hh: Int
+    tasks(key: String): [Task]
+  }
+`;
+ 
+const resolvers = {
+  Query: {
+    hello: () => `({
+      name: '111'
+    })`,
+    hh: () => 1,
+    tasks(a: any, o: any) {
+      return [
+        {
+          name: o.key,
+          time: 12345
+        }
+      ];
+    }
+  }
+};
+ 
+const server = new ApolloServer({ typeDefs, resolvers,
+  tracing: true });
+ 
+const app = new Koa();
+const port = 4000;
+server.applyMiddleware({ app });
+ 
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
+);
