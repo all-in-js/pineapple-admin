db7538bc67eb8d7731d17167dc8f79771171be1a
famanoder <915905174@qq.com>
2021-02-03 18:57:38 +0800

    Delete server/dist directory

--- server/dist/models/projects.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function findProjects(filterKeys) {
-    const query = {};
-    if (!filterKeys) {
-    }
-}
-exports.findProjects = findProjects;
--- server/dist/models/svgs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*
- * @Author: your name
- * @Date: 2021-01-06 11:44:55
- * @LastEditTime: 2021-01-06 11:45:12
- * @LastEditors: your name
- * @Description: In User Settings Edit
- * @FilePath: \opensources\pineapple-admin\server\models\svgs.ts
- */
